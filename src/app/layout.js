98368d5b93901e0afd367747f80fc29f71a3b9e7
arnavnagpurkar <arnavn.dev@gmail.com>
2024-03-22 12:53:43 +0530

    Deployment issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
@@ -9,10 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
+      <Head>
+        <script src="https://cdn.lordicon.com/lordicon.js"></script>
+      </Head>
       <body className="dark:bg-zinc-950 dark:text-white">
         <Navbar />
         {children}
-        <script src="https://cdn.lordicon.com/lordicon.js"></script>
       </body>
     </html>
   );
